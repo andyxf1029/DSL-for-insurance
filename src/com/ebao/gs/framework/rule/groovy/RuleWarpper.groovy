2589c2864e17ec51bded4dca8531d0ba3c235c96
yi.feng <andyxf1029@gmail.com>
2014-02-18 03:11:50 +0800

    add method

--- src/com/ebao/gs/framework/rule/groovy/RuleWarpper.groovy
+++ src/com/ebao/gs/framework/rule/groovy/RuleWarpper.groovy
@@ -1,10 +1,10 @@
+import com.ebao.bean.PolicyCT
 import com.ebao.gs.framework.rule.service.context.IContextDataServiceWarpper
 
 
 def run(closure,context){
 	def ruleWarpper = new RuleMethodWarpper();
-	ruleWarpper.mergeMethods(closure,context);
-	ruleWarpper.dataService=dataService
+	ruleWarpper.mergeMethods(closure,context,dataService);
 	closure.run();
 }
 
@@ -14,8 +14,10 @@ def run(closure,context){
 
 class RuleMethodWarpper {
 	IContextDataServiceWarpper dataService
-	
-	def mergeMethods(closure,context){
+
+	def mergeMethods(closure,context,dataService){
+		this.dataService= dataService
+		
 		closure.metaClass.propertyMissing = {String name ->
 			dataService.fetchData(context,name);
 		}
@@ -24,8 +26,41 @@ class RuleMethodWarpper {
 			dataService.updateValue(context,name,value);
 		}
 
-
-
+		closure.metaClass.coverage = {String name ->
+			PolicyCT ct = new PolicyCT();
+			ct.code=name
+			def ruleWarpper = new RuleMethodWarpper();
+			context.put("currentCtCode",name);
+			ruleWarpper.mergeMethods(ct, context,dataService)
+			return ct;
+		}
+		
+		closure.metaClass.coverageArray = {String name ->
+			List<PolicyCT> ctList = new ArrayList<PolicyCT> ()
+			PolicyCT ct = new PolicyCT();
+			ctList.add(ct);
+			def ruleWarpper = new RuleMethodWarpper();
+			context.put("currentCtCode",name);
+			ruleWarpper.mergeMethods(ct, context,dataService)
+			return ctList;
+		}
+		
+		closure.metaClass.sum = {List lists ->
+			
+			
+			return lists;
+		}
+		
+		
+		closure.metaClass.stop = {String code ->
+			throw new Exception(code);
+		}
+		
+		closure.metaClass.warn = {String code ->
+			return code
+		}
+		
+		
 		//TODO
 
 		return closure;
